545237e7ef21df4cdb1a4003fc6661a6b18c3077
De Wet Blomerus <dewet@blomerus.org>
2021-08-07 04:36:00 -0400

    recursive awesomeness to get the average

--- lib/quick_average_web/average_live_state.ex
+++ lib/quick_average_web/average_live_state.ex
@@ -1,28 +1,21 @@
 defmodule QuickAverageWeb.AverageLive.State do
-  def list_users(presence_list) do
-    Map.values(presence_list)
-    |> Enum.map(fn u ->
-      [head | _] = u.metas
-      head
-    end)
-  end
-
   def average(presence_list) do
-    numbers =
-      list_users(presence_list)
-      |> Enum.map(& &1.number)
-      |> Enum.filter(&(!is_nil(&1)))
-
-    calculate_average(numbers)
+    presence_list |> Map.values() |> average(0, 0)
   end
 
-  defp calculate_average([]) do
-    nil
+  defp average([], 0, 0), do: nil
+
+  defp average([], total, length) do
+    (total / length) |> Float.round(2)
   end
 
-  defp calculate_average(numbers) do
-    (Enum.sum(numbers) / Enum.count(numbers))
-    |> Float.round(2)
+  defp average([%{metas: [%{number: number}]} | tail], total, length)
+       when not is_nil(number) do
+    average(tail, total + number, length + 1)
+  end
+
+  defp average([_ | tail], total, length) do
+    average(tail, total, length)
   end
 
   def parse_number(number_input) do
--- test/quick_average_web/average_live_state_test.exs
+++ test/quick_average_web/average_live_state_test.exs
@@ -35,33 +35,6 @@ defmodule QuickAverageWeb.AverageLive.StateTest do
     }
   }
 
-  @users_list [
-    %{
-      name: "Darth",
-      number: 6.0,
-      phx_ref: "FpFUFvXLJpz5pAOF",
-      phx_ref_prev: "FpFUFl0T65P5pAeI"
-    },
-    %{
-      name: "Luke",
-      number: 10.0,
-      phx_ref: "FpFUFZZeaaj5pASE",
-      phx_ref_prev: "FpFUFWgobQ35pALH"
-    },
-    %{
-      name: "De Wet",
-      number: 9.0,
-      phx_ref: "FpFUFJ9yrlL5pAaD",
-      phx_ref_prev: "FpFUFGgOMyb5pAZD"
-    }
-  ]
-
-  describe("list_users/1") do
-    test "lists the users" do
-      assert State.list_users(@presence_list) == @users_list
-    end
-  end
-
   describe("average/1") do
     test "lists the users" do
       assert State.average(@presence_list) == 8.33
