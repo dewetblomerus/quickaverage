72b3253b0c227c5f6ab11b6d98cb51e1e3255860
De Wet Blomerus <dewet@blomerus.org>
2021-09-18 08:21:29 -0500

    basic auth to live dashboard

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,9 @@
 use Mix.Config
 
+config :quick_average,
+  username: "asdf",
+  password: "asdf"
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :quick_average, QuickAverageWeb.Endpoint,
