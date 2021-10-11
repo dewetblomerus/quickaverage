fc4174dcf0e56c5f807f99c191b69e1d6ba02836
De Wet Blomerus <dewet@blomerus.org>
2021-10-11 16:53:22 -0400

    remove config comments 🔥

--- config/dev.exs
+++ config/dev.exs
@@ -4,12 +4,6 @@ config :quick_average,
   username: "asdf",
   password: "asdf"
 
-# For development, we disable any cache and enable
-# debugging and code reloading.
-#
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with webpack to recompile .js and .css sources.
 config :quick_average, QuickAverageWeb.Endpoint,
   http: [port: 4000],
   https: [
@@ -64,12 +58,8 @@ config :quick_average, QuickAverageWeb.Endpoint,
     ]
   ]
 
-# Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
-# Set a higher stacktrace during development. Avoid configuring such
-# in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
-# Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
--- config/releases.exs
+++ config/releases.exs
@@ -1,7 +1,3 @@
-# In this file, we load production configuration and secrets
-# from environment variables. You can also hardcode secrets,
-# although such is generally not recommended and you have to
-# remember to add this file to your .gitignore.
 import Config
 
 http_port = 80
@@ -23,13 +19,3 @@ config :quick_average, QuickAverageWeb.Endpoint,
   ],
   secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
   url: [host: System.fetch_env!("HOST"), port: https_port]
-
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :quick_average, QuickAverageWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
