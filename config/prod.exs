807a1a544e92cb5e39b33a9bc2cd33d63e1b828d
De Wet Blomerus <dewet@blomerus.org>
2021-10-11 16:58:30 -0400

    move static prod config to prod 🎛

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,19 @@ use Mix.Config
 # to something meaningful, Phoenix uses this information
 # when generating URLs.
 
+http_port = 80
+https_port = 443
+
 config :quick_average, QuickAverageWeb.Endpoint,
+  http: [
+    port: http_port,
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  https: [
+    port: https_port,
+    cipher_suite: :strong,
+    transport_options: [socket_opts: [:inet6]]
+  ],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [hsts: true],
   server: true
--- config/releases.exs
+++ config/releases.exs
@@ -1,21 +1,11 @@
 import Config
 
-http_port = 80
-https_port = 443
-
 config :quick_average,
   username: System.fetch_env!("ADMIN_USERNAME"),
   password: System.fetch_env!("ADMIN_PASSWORD")
 
+https_port = 443
+
 config :quick_average, QuickAverageWeb.Endpoint,
-  http: [
-    port: http_port,
-    transport_options: [socket_opts: [:inet6]]
-  ],
-  https: [
-    port: https_port,
-    cipher_suite: :strong,
-    transport_options: [socket_opts: [:inet6]]
-  ],
   secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
   url: [host: System.fetch_env!("HOST"), port: https_port]
