c5cb0c69554d541769ba9073c21f3b119a68706e
De Wet Blomerus <dewet@blomerus.org>
2021-10-11 21:47:02 -0500

    Revert "remove unused assigns 🔥"
    This reverts commit 36f0689dbcea7085c6cf613ba64ab8c88b60113c.

--- lib/quick_average_web/live/average_live.ex
+++ lib/quick_average_web/live/average_live.ex
@@ -173,7 +173,14 @@ defmodule QuickAverageWeb.AverageLive do
   def handle_info("clear", socket) do
     send(self(), "clear_number_front")
 
-    room_update(socket, %{number: nil})
+    room_update(
+      socket,
+      %{
+        only_viewing: socket.assigns.only_viewing,
+        name: socket.assigns.name,
+        number: nil
+      }
+    )
 
     {:noreply, assign(socket, number: nil, reveal_by_click: false)}
   end
