0a7bde73e221692d5f83735c0b2cdbe806757e93
De Wet Blomerus <dewet@blomerus.org>
2021-10-20 11:05:48 -0400

    more clean benchmark code 🧹

--- lib/quick_average_web/benchmark/benchmark_live.ex
+++ lib/quick_average_web/benchmark/benchmark_live.ex
@@ -2,6 +2,7 @@ defmodule QuickAverageWeb.BenchmarkLive do
   require IEx
   use QuickAverageWeb, :live_view
   alias QuickAverageWeb.Supervisor.Interface, as: SupervisorInterface
+  @default_refresh_interval 500
 
   @impl true
   def mount(_params, _session, socket) do
@@ -23,34 +24,37 @@ defmodule QuickAverageWeb.BenchmarkLive do
         },
         socket
       ) do
-    number_of_clients = parse_number(input_number_of_clients)
-    refresh_interval = parse_number(input_refresh_interval)
-
-    new_assigns = %{
+    supervisor_params = %{
       room_id: room_id,
-      number_of_clients: parse_number(number_of_clients),
-      refresh_interval: parse_number(refresh_interval)
+      number_of_clients: parse_number(input_number_of_clients),
+      refresh_interval:
+        parse_number(input_refresh_interval, @default_refresh_interval)
     }
 
-    SupervisorInterface.update(new_assigns)
+    SupervisorInterface.update(supervisor_params)
 
     {:noreply,
      assign(
        socket,
-       new_assigns
+       room_id: room_id,
+       number_of_clients: input_number_of_clients,
+       refresh_interval: input_refresh_interval
      )}
   end
 
-  def parse_number(nil), do: nil
+  def parse_number(_, default \\ 0)
 
-  def parse_number(input_number) when is_binary(input_number) do
+  def parse_number(nil, default), do: default
+  def parse_number("", default), do: default
+
+  def parse_number(input_number, default) when is_binary(input_number) do
     case Integer.parse(input_number) do
       {number, ""} -> number
-      _ -> nil
+      _ -> default
     end
   end
 
-  def parse_number(input_number) when is_integer(input_number) do
+  def parse_number(input_number, _) when is_integer(input_number) do
     input_number
   end
 end
--- lib/quick_average_web/benchmark/supervisor_interface.ex
+++ lib/quick_average_web/benchmark/supervisor_interface.ex
@@ -4,46 +4,26 @@ defmodule QuickAverageWeb.Supervisor.Interface do
 
   @supervisor QuickAverageWeb.BenchmarkSupervisor
 
-  def update(
-        %{
-          room_id: room_id,
-          number_of_clients: input_number,
-          refresh_interval: refresh_interval
-        } = params
-      ) do
-    desired_number = parse_number(input_number)
+  def update(%{number_of_clients: desired_number} = params) do
     children_number_diff = desired_number - Enum.count(children())
 
     adjust(params, children_number_diff)
   end
 
-  def parse_number(nil), do: 0
-  def parse_number(""), do: 0
-  def parse_number(input_number), do: input_number
-
   def adjust(
         %{
           room_id: room_id,
-          number_of_clients: desired_number,
           refresh_interval: refresh_interval
-        } = params,
+        },
         diff
       )
       when diff > 0 do
-    Logger.info(diff, label: "diff")
+    Logger.info(Integer.to_string(diff), label: "diff")
     1..diff |> Enum.each(fn _ -> create(room_id, refresh_interval) end)
   end
 
-  def adjust(
-        %{
-          room_id: room_id,
-          number_of_clients: desired_number,
-          refresh_interval: refresh_interval
-        } = params,
-        diff
-      )
-      when diff < 0 do
-    Logger.info(diff, label: "diff")
+  def adjust(_, diff) when diff < 0 do
+    Logger.info(Integer.to_string(diff), label: "diff")
     -1..diff |> Enum.each(fn _ -> delete() end)
   end
 
@@ -51,7 +31,7 @@ defmodule QuickAverageWeb.Supervisor.Interface do
     Logger.info("desired number reached")
   end
 
-  def create(room_id, refresh_interval) do
+  def create(room_id, refresh_interval) when is_integer(refresh_interval) do
     DynamicSupervisor.start_child(
       @supervisor,
       {User, {room_id, refresh_interval}}
@@ -63,7 +43,6 @@ defmodule QuickAverageWeb.Supervisor.Interface do
   end
 
   def delete do
-    # IEx.pry()
     [{_, pid, _, _} | _] = DynamicSupervisor.which_children(@supervisor)
     DynamicSupervisor.terminate_child(@supervisor, pid)
   end
