bc3e08875bf2bd487cd8fd7117d394e4246f249a
De Wet Blomerus <dewet.blomerus@salesloft.com>
2022-07-09 17:46:42 -0400

    do everything I think I need for fly

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,6 @@ import Config
 
 config :quick_average, QuickAverageWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [hsts: true],
   server: true
 
 config :logger, level: :info
--- config/releases.exs
+++ config/releases.exs
@@ -1,29 +1,52 @@
 import Config
 
-use_https =
-  System.get_env("USE_HTTPS", "true")
-  |> String.contains?("true")
-
 config :quick_average,
   username: System.fetch_env!("ADMIN_USERNAME"),
-  password: System.fetch_env!("ADMIN_PASSWORD"),
-  use_https: use_https
+  password: System.fetch_env!("ADMIN_PASSWORD")
 
-port = System.get_env("PORT", "443") |> String.to_integer()
+if config_env() == :prod do
+  # The secret key base is used to sign/encrypt cookies and other secrets.
+  # A default value is used in config/dev.exs and config/test.exs but you
+  # want to use a different value for prod and you most likely don't want
+  # to check this value into version control, so we use an environment
+  # variable instead.
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
 
-config :quick_average, QuickAverageWeb.Endpoint,
-  secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
-  url: [host: System.fetch_env!("HOST"), port: port],
-  http: [
-    port: 80,
-    transport_options: [socket_opts: [:inet6]]
-  ]
+  host = System.get_env("PHX_HOST") || "quickaverage.com"
+  port = String.to_integer(System.get_env("PORT") || "4000")
 
-if use_https do
   config :quick_average, QuickAverageWeb.Endpoint,
-    https: [
-      port: 443,
-      cipher_suite: :strong,
-      transport_options: [socket_opts: [:inet6]]
-    ]
+    url: [host: host, port: 443, scheme: "https"],
+    http: [
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port
+    ],
+    secret_key_base: secret_key_base
+
+  # ## Configuring the mailer
+  #
+  # In production you need to configure the mailer to use a different adapter.
+  # Also, you may need to configure the Swoosh API client of your choice if you
+  # are not using SMTP. Here is an example of the configuration:
+  #
+  #     config :sixteen, Sixteen.Mailer,
+  #       adapter: Swoosh.Adapters.Mailgun,
+  #       api_key: System.get_env("MAILGUN_API_KEY"),
+  #       domain: System.get_env("MAILGUN_DOMAIN")
+  #
+  # For this example you need include a HTTP client required by Swoosh API client.
+  # Swoosh supports Hackney and Finch out of the box:
+  #
+  #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
+  #
+  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
 end
