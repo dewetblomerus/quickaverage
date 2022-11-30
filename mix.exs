f4055bbe162388a2feb2842736b302cee4b8965b
De Wet Blomerus <dewet.blomerus@salesloft.com>
2022-11-30 22:39:49 +0200

    all changes from https://elixircasts.io/upgrading-to-phoenix-1.7 upto but not yet verified routes

--- mix.exs
+++ mix.exs
@@ -7,7 +7,6 @@ defmodule QuickAverage.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -43,6 +42,7 @@ defmodule QuickAverage.MixProject do
       {:phoenix_live_dashboard, "~> 0.7"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.18.3"},
+      {:phoenix_view, "~> 2.0"},
       {:phoenix, "~> 1.7.0-rc.0", override: true},
       {:plug_cowboy, "~> 2.6"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
